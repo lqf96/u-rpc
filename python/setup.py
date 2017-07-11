135f3977c26ecf9f8cf840f975e440a45652a63b
Qifan Lu <lqf.1996121@gmail.com>
2017-07-11 13:43:15 -0700

    small fixes

--- python/setup.py
+++ python/setup.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python
+from __future__ import unicode_literals
 from setuptools import setup
 
 setup(
     name="u-rpc",
     version="0.1.0",
-    author="email",
+    author="lqf96",
     author_email="lqf.1996121@gmail.com",
     description="The u-RPC Remote Procedure Call framework",
     license="MIT",
--- python/urpc/python.py
+++ python/urpc/python.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from collections import Container
 from abc import ABCMeta, abstractmethod
 from six import text_type, with_metaclass
