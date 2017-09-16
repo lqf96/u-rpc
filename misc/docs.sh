6cb1569bae1be168d1faf00a9b3f6fe275798808
Qifan Lu <lqf.1996121@gmail.com>
2017-09-16 12:37:01 +0800

    Remove old documents & add related repo upload tool

---
+++ misc/docs.sh
@@ -0,0 +1,21 @@
+# Repository URL
+REPO_URL="ssh://github.com/lqf96/u-rpc"
+# Repository branch
+REPO_BRANCH="gh-pages"
+
+# Source
+SOURCE_DIR="../docs"
+
+# Before upload hook
+before_upload_hook() {
+    # Create directory
+    mkdir -p "${SOURCE_DIR}"
+    # Generate documents
+    (cd ../c; doxygen)
+    (cd ../python; doxygen)
+}
+# After upload hook
+after_upload_hook() {
+    # Remove directory
+    rm -rf "${SOURCE_DIR}"
+}
